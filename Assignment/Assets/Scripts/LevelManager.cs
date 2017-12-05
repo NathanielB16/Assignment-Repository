ebdf1bb9205c99d5336ff19c360d7d5fc086ece2
Nathaniel Bugeja <b42137@mcast.org.mt>
2017-12-05 09:44:20 +0100

    Creating the Start and Level_01 scenes, and making the start button work.

---
+++ Assignment/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadTheLevel(string NameOfLevel)
+    {
+        SceneManager.LoadScene(NameOfLevel);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void LoadTheNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
