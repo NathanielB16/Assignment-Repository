3d0ff771156dc9f18349a0296138f25ff566dade
NathanielB16 <32700278+NathanielB16@users.noreply.github.com>
2017-12-08 13:22:33 +0100

    Creating the Level_01 GUI and attaching the necessary scripts and materials.
    Creating the Level_01 GUI and attaching the necessary scripts and materials.

---
+++ Assignment/Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Player Players;
+
+    private Vector3 PlayerToBallVector;
+
+    private bool Started = false;
+
+    float randomX, randomY;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        randomX = Random.Range(0f, 0.5f);
+        randomY = Random.Range(0f, 0.5f);
+
+        Vector2 adjust = new Vector2(randomX, randomY);
+
+        this.GetComponent<Rigidbody2D>().velocity += adjust;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        Players = GameObject.FindObjectOfType<Player>();
+
+        PlayerToBallVector = this.transform.position - Players.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Started == false)
+        {
+
+        }
+
+    }
+}
---
+++ Assignment/Assets/Scripts/Players.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour {
+
+    private Ball myBall;
+
+	// Use this for initialization
+	void Start () {
+
+        myBall = GameObject.FindObjectOfType<Ball>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float PosMouseInUnits = (Input.mousePosition.x/Screen.width * 13f) - 6.5f;
+
+        float PosOfMouse = Mathf.Clamp(myBall.transform.position.x, -6f, 6f);
+
+        Vector3 PlayerPos = new Vector3(PosOfMouse, this.transform.position.y, 0);
+
+        this.transform.position = PlayerPos;
+		
+	}
+}
